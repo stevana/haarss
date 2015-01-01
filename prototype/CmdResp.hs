e3886174a6e5f0ab8d6486ef1a8b7d8e7f2892ec
Stevan Andjelkovic <stevan.andjelkovic@strath.ac.uk>
2015-01-01 18:44:16 +0100

    Further tweak the Haskell interaction prototype.

--- prototype/CmdResp.hs
+++ prototype/CmdResp.hs
@@ -1,61 +1,86 @@
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE PolyKinds #-}
 {-# LANGUAGE GADTs #-}
 {-# LANGUAGE DataKinds #-}
 
+import Control.Applicative
+
 import FRP.Sodium
 import FRP.Sodium.IO
 
-data Op = Move | Fetch
+data Op = Move | Fetch | Ask | PutChar | Done
+
+data OP :: Op -> * where
+  MOVE    :: OP Move
+  FETCH   :: OP Fetch
+  ASK     :: OP Ask
+  PUTCHAR :: OP PutChar
+  DONE    :: OP Done
+
+data Prompt = AddFeed | Search
 
 data Dir = Up | Down
 
-data Cmd :: Op -> * where
-  MoveP  :: Dir -> Cmd Move
-  FetchP :: Cmd Fetch
+type family Cmd (o :: Op) :: * where
+  Cmd 'Move    = Dir
+  Cmd 'Fetch   = ()
+  Cmd 'Ask     = Prompt
+  Cmd 'PutChar = Char
+  Cmd 'Done    = ()
 
-data Resp :: Op -> * where
-  MoveA  :: Resp Move
-  FetchA :: String -> Resp Fetch
+type family Resp (o :: Op) :: * where
+  Resp 'Move    = ()
+  Resp 'Fetch   = String
+  Resp 'Ask     = ()
+  Resp 'PutChar = ()
+  Resp 'Done    = ()
+
+data ExCmd = forall o. ExCmd (OP o) (Cmd o)
 
 data VtyEvent = Key Char | Enter
 
 eEvent :: Event VtyEvent
 eEvent = undefined
 
-data Ex (p :: k -> *) where
-  Ex :: p i -> Ex p
+data Mode = CommandMode | InputMode
 
-eCmd :: Event (Ex Cmd)
-eCmd = filterJust $ fmap cmd eEvent
+eCmd' :: Reactive (Event (Maybe ExCmd))
+eCmd' = collectE cmd CommandMode eEvent
   where
-  cmd :: VtyEvent -> Maybe (Ex Cmd)
-  cmd (Key 'j') = Just $ Ex (MoveP Down)
-  cmd (Key 'k') = Just $ Ex (MoveP Up)
-  cmd (Key 'u') = Just $ Ex FetchP
-  cmd _         = Nothing
+  cmd :: VtyEvent -> Mode -> (Maybe ExCmd, Mode)
+  cmd (Key 'j') CommandMode = (Just $ ExCmd MOVE Down,   CommandMode)
+  cmd (Key 'k') CommandMode = (Just $ ExCmd MOVE Up,     CommandMode)
+  cmd (Key 'u') CommandMode = (Just $ ExCmd FETCH (),    CommandMode)
+  cmd (Key 'a') CommandMode = (Just $ ExCmd ASK AddFeed, InputMode)
+  cmd _         CommandMode = (Nothing,                  CommandMode)
+  cmd Enter     InputMode   = (Just $ ExCmd DONE (),     CommandMode)
+  cmd (Key c)   InputMode   = (Just $ ExCmd PUTCHAR c,   InputMode)
 
-resp :: Cmd o -> IO (Resp o)
-resp (MoveP _) = return MoveA
-resp FetchP    = return (FetchA "apa")
+eCmd :: Event ExCmd
+eCmd = undefined
 
-data (:*) (p :: k -> *) (q :: k -> *) :: k -> * where
-  (:&) :: p i -> q i -> (p :* q) i
+resp :: OP o -> Cmd o -> IO (Resp o)
+resp MOVE  _  = return ()
+resp FETCH () = return "apa"
+resp ASK   _  = return ()
 
-eResp :: Event (Ex (Cmd :* Resp))
-eResp = executeAsyncIO $ fmap (\(Ex p) -> do
-  a <- resp p
-  return (Ex (p :& a))) eCmd
+data ExResp = forall o. ExResp (OP o) (Cmd o) (Resp o)
 
+eResp :: Event ExResp
+eResp = executeAsyncIO $ fmap
+  (\(ExCmd o p) -> ExResp o p <$> resp o p) eCmd
 data Model = Model String
 
 initModel :: Model
 initModel = Model ""
 
 rModel :: Reactive (Behaviour Model)
-rModel = accum initModel (fmap (\(Ex (p :& a)) -> update p a) eResp)
+rModel = accum initModel (fmap (\(ExResp o p a) -> update o p a) eResp)
   where
-  update :: Cmd o -> Resp o -> Model -> Model
-  update (MoveP _)    MoveA     m = m
-  update  FetchP     (FetchA s) _ = Model s
-  update _           _          _ = error "Impossible."
+  update :: OP o -> Cmd o -> Resp o -> Model -> Model
+  update MOVE  d   () m = m
+  update FETCH ()  s  _ = Model s
+  update ASK   p  ()  m = m
