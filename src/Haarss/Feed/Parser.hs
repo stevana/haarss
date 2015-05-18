77dd2f417852ceec0cad1c7607c40980444a92f5
Stevan Andjelkovic <stevan.andjelkovic@strath.ac.uk>
2015-05-18 13:59:26 +0200

    Revert parser change.

--- src/Haarss/Feed/Parser.hs
+++ src/Haarss/Feed/Parser.hs
@@ -3,15 +3,16 @@
 
 module Haarss.Feed.Parser (parseFeed) where
 
+import           Control.Applicative
 import           Control.DeepSeq
 import           Control.Exception       (SomeException)
-import           Control.Lens            hiding (element)
 import           Data.ByteString.Lazy    (ByteString)
 import           Data.Monoid
 import           Data.Text               (Text)
 import qualified Data.Text               as T
-import           Text.XML
-import           Text.XML.Cursor
+import           Data.Text.Lens          (unpacked)
+import           Text.XML                (def, parseLBS)
+import           Text.XML.Lens
 
 import           Haarss.Feed.Feed
 
@@ -22,15 +23,15 @@ import           Haarss.Feed.Feed
 
 parseFeed :: ByteString -> Either SomeException Feed
 parseFeed bs = do
-  doc <- parseLBS (def { psDecodeEntities = decodeHtmlEntities }) bs
-  deepseq doc $ fromXML (fromDocument doc)
-                        (nameLocalName (elementName (documentRoot doc)))
+  doc <- parseLBS def bs
+  deepseq doc $ fromXML doc
 
-fromXML :: Cursor -> Text -> Either SomeException Feed
-fromXML c "RDF"  = return $ fromRSS RSS1Kind c itemsRSS1
-fromXML c "rss"  = return $ fromRSS RSS2Kind c itemsRSS2
-fromXML c "feed" = return $ fromAtom c
-fromXML _ _      = fail   $ error "fromXML: unknown feed kind."
+fromXML :: Document -> Either SomeException Feed
+fromXML doc = case doc^.root.localName of
+  "RDF"  -> return $ fromRSS1 doc
+  "rss"  -> return $ fromRSS2 doc
+  "feed" -> return $ fromAtom doc
+  _      -> fail   $ error "fromXML: unknown feed kind."
 
 process :: Text -> Text
 process = T.strip
@@ -40,79 +41,59 @@ process = T.strip
         . filter (not . T.null)
         . T.lines
 
-concatMaybe :: Monoid m => [m] -> Maybe m
-concatMaybe ts = case ts of
-  []  -> Nothing
-  _:_ -> Just (mconcat ts)
-
-fromRSS :: FeedKind -> Cursor -> (Cursor -> [Item]) -> Feed
-fromRSS k c is = Feed
-  { _feedKind        = k
-  , _feedTitle       = concatMaybe $ c $//
-                         element "channel" &/ element "title" &// content
-  , _feedHome        = ""
-  , _feedHTML        = ""
-  , _feedDescription = concatMaybe $ c $//
-                         element "channel" &/
-                         element "description" &// content
-  , _feedLastUpdate  = concatMaybe $ c $//
-                         element "channel" &/ element "date" &// content
-  , _feedItems       = is c
-  }
-
-itemsRSS1 :: Cursor -> [Item]
-itemsRSS1 c = c $// element "item" >=> toItem
+fromRSS1 :: Document -> Feed
+fromRSS1 doc = newEmptyFeed RSS1Kind
+  & feedTitle       .~ doc^?root./ell "channel"./ell "title".text
+  & feedHome        .~ ""
+  & feedHTML        .~ ""
+  & feedDescription .~ doc^?root.ell "channel"./ell "description".text
+  & feedLastUpdate  .~ doc^?root.ell "channel"./ell "date".text
+  & feedItems       .~ doc^..root./ell "item" & mapped.mapped %~ toItem
   where
-  toItem :: Cursor -> [Item]
-  toItem c' = return $ Item
-    { _itemTitle       = concatMaybe $ c' $// element "title" &// content
-    , _itemLink        = concatMaybe $ c' $// element "link"
-                                          &// map T.unpack . content
-    , _itemDate        = Nothing
-    , _itemFeedLink    = Nothing
-    , _itemDescription = concatMaybe $ c' $//
-                           element "description" &// content
-    }
+  toItem :: Element -> Item
+  toItem e = newEmptyItem
+    & itemTitle       .~ (e^?entire.ell "title".text & mapped %~ process)
+    & itemLink        .~ e^?entire.ell "link".text.unpacked
+    & itemDate        .~ Nothing
+    & itemFeedLink    .~ Nothing
+    & itemDescription .~ e^?entire.ell "description".text
 
-itemsRSS2 :: Cursor -> [Item]
-itemsRSS2 c = c $// element "channel" &/ element "item" >=> toItem
+fromRSS2 :: Document -> Feed
+fromRSS2 doc = newEmptyFeed RSS2Kind
+  & feedTitle       .~ doc^?root./el "channel"./el "title".text
+  & feedHome        .~ ""
+  & feedHTML        .~ ""
+  & feedDescription .~ doc^?root.el "channel"./el "description".text
+  & feedLastUpdate  .~ doc^?root.el "channel"./el "date".text
+  & feedItems       .~ doc^..root./el "channel"./el "item"
+                         & mapped.mapped %~ toItem
   where
-  toItem :: Cursor -> [Item]
-  toItem c' = return $ Item
-    { _itemTitle       = concatMaybe $ c' $// element "title" &// content
-    , _itemLink        = concatMaybe $ c' $// element "link"
-                                          &// map T.unpack . content
-    , _itemDate        = concatMaybe $ c' $// element "pubDate"
-                                          &// content
-    , _itemFeedLink    = Nothing
-    , _itemDescription = concatMaybe $ c' $//
-                           element "description" &// content
-    }
+  toItem :: Element -> Item
+  toItem e = newEmptyItem
+    & itemTitle       .~ (e^?entire.el "title".text & mapped %~ process)
+    & itemLink        .~ e^?entire.el "link".text.unpacked
+    & itemDate        .~ e^?entire.el "pubDate".text
+    & itemFeedLink    .~ Nothing
+    & itemDescription .~ e^?entire.el "description".text
 
-fromAtom :: Cursor -> Feed
-fromAtom c = Feed
-  { _feedKind        = AtomKind
-  , _feedTitle       = concatMaybe $ c $// element "title" &// content
-  , _feedHome        = ""
-  , _feedHTML        = ""
-  , _feedDescription = Nothing
-  , _feedLastUpdate  = concatMaybe $ c $// element "updated" &// content
-  , _feedItems       = c $// element "entry" >=> toItem
-  }
+fromAtom :: Document -> Feed
+fromAtom doc = newEmptyFeed AtomKind
+  & feedTitle       .~ doc^?root./ell "title".text
+  & feedHome        .~ ""
+  & feedHTML        .~ ""
+  & feedDescription .~ Nothing
+  & feedLastUpdate  .~ doc^?root.ell "updated".text
+  & feedItems       .~ doc^..root./ell "entry" & mapped.mapped %~ toItem
   where
-  toItem :: Cursor -> [Item]
-  toItem c' = return $ Item
-    { _itemTitle       = concatMaybe $ c' $// element "title" &// content
-    , _itemLink        = concatMaybe $ c' $// element "link"
-                                          &// map T.unpack . attribute "href"
-    , _itemDate        = concatMaybe $
-                           (c' $// element "published" &// content) ++
-                           (c' $// element "updated"   &// content)
-    , _itemFeedLink    = Nothing
-    , _itemDescription = fmap removeHtml $ concatMaybe $
-                           (c' $// element "summary" &// content) ++
-                           (c' $// element "content" &// content)
-    }
+  toItem :: Element -> Item
+  toItem e = newEmptyItem
+    & itemTitle       .~ (e^?entire.ell "title".text & mapped %~ process)
+    & itemLink        .~ e^?entire.ell "link".attr "href".unpacked
+    & itemDate        .~ (e^?entire.ell "published".text <|>
+                          e^?entire.ell "updated".text)
+    & itemFeedLink    .~ Nothing
+    & itemDescription .~ e^?entire.ell "summary".text <>
+                         e^?entire.ell "content".text.to removeHtml
     where
     removeHtml :: Text -> Text
     removeHtml = go False . T.strip
@@ -120,11 +101,13 @@ fromAtom c = Feed
       go :: Bool -> Text -> Text
       go False (T.uncons -> Nothing)       = T.empty
       go False (T.uncons -> Just ('<', t)) = go True  t
-      go False (T.uncons -> Just (x, t))   = x `T.cons` go False t
+      go False (T.uncons -> Just (c, t))   = c `T.cons` go False t
       go True  (T.uncons -> Nothing)       = T.empty
       go True  (T.uncons -> Just ('>', t)) = go False  t
       go True  (T.uncons -> Just (_, t))   = go True t
-      go _     (T.uncons -> _)             = error "Impossible"
+      go False (T.uncons -> _)             = error "Impossible"
+      go True  (T.uncons -> _)             = error "Impossible"
+
 
 ------------------------------------------------------------------------
 -- XXX: Debugging
